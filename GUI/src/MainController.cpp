000ff8c65bb0087609e0595eaec57a64cb9e66dd
tianwailaike <wjlyctobeone@163.com>
2017-02-28 08:48:42 +0800

    live mode

--- GUI/src/MainController.cpp
+++ GUI/src/MainController.cpp
@@ -34,8 +34,8 @@ MainController::MainController(int argc, char * argv[])
     std::string calibrationFile;
     Parse::get().arg(argc, argv, "-cal", calibrationFile);
 
-     Resolution::getInstance(640, 480);
-    // Resolution::getInstance(512,424);
+     //Resolution::getInstance(640, 480);
+        Resolution::getInstance(512,424);
 
     if(calibrationFile.length())
     {
@@ -43,8 +43,8 @@ MainController::MainController(int argc, char * argv[])
     }
     else
     {
-        Intrinsics::getInstance(528, 528, 320, 240);
-       // Intrinsics::getInstance(364.531494,364.531494,256.237396,207.297302);
+        //Intrinsics::getInstance(528, 528, 320, 240);
+        Intrinsics::getInstance(364.531494,364.531494,256.237396,207.297302);
         
     }
 
